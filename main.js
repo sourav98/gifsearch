762f8e8523e86ef37471058bb75fa80e4aac012a
Sourav Sudhi <sourav.sudhi@gmail.com>
2020-04-23 20:10:28 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,48 @@
+
+/* 1. Grab the input*/ 
+
+    document.querySelector(".button-container").addEventListener('click',function(e){
+        var input=document.querySelector(".input-container").value;
+        searchGify( input );
+    });
+    
+    document.querySelector(".input-container").addEventListener('keyup',function(e){
+            if(e.keyCode===13){
+                var input=document.querySelector(".input-container").value;
+                searchGify(input );
+            }         
+    });
+
+    
+/* 2. Search gifs and fetch it from api */
+
+    function searchGify(searchQuery)
+    {
+    var url = "http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=" + searchQuery;
+    var gifyAjaxCall = new XMLHttpRequest();
+    gifyAjaxCall.open('GET',url);
+    gifyAjaxCall.send();
+
+    gifyAjaxCall.addEventListener('load',function(data)
+    {
+        var actualData= data.target.response;
+        pushToDom(actualData); 
+    });
+}
+
+/* 3. Show me the gifs*/ 
+
+    function pushToDom(input){
+        var response=JSON.parse(input);
+        var imageUrls=response.data;
+        var container=document.querySelector('.jscontainer');
+       container.innerHTML="";
+       
+        // loop through the array 
+        imageUrls.forEach(function(image){
+            var src=image.images.fixed_height.url;
+            container.innerHTML+= "<img src=\"" +src+ "\" class=\"container-image\">";
+        });
+       
+    }
+
